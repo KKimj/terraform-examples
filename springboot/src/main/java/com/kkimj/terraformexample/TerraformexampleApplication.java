4bd4f6664549c3c3748570af3e8938cbafa128c3
Jiun Kim <kkimj@hanyang.ac.kr>
2023-02-06 00:50:37 +0900

    feat: Update to add Spring boot project

---
+++ springboot/src/main/java/com/kkimj/terraformexample/TerraformexampleApplication.java
@@ -0,0 +1,13 @@
+package com.kkimj.terraformexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraformexampleApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/com/kkimj/terraformexample/TerraformexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkimj.terraformexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
